cefec7f865315e2703564c9b0714a2a82577a718
shahrooz <sibche35@gmail,com>
2019-04-07 11:28:56 +0430

    added a function to get only images

--- bot_dog.py
+++ bot_dog.py
@@ -10,8 +10,17 @@ def get_url():
     url = contents['url']
     return url 
 
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
+
 def bop(bot, update):
-    url = get_url()
+    url = get_image_url()
     chat_id = update.message.chat_id 
     bot.send_photo(chat_id=chat_id, photo=url)
 
